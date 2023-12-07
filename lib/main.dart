dd16357f81690ccb8bd098967271c529dc4fb9d1
ts30727 <>
2023-12-07 15:40:32 -0700

    Navigation
    Designed and implemented main navigation scheme. Also cleaned up some comments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:dart_assistant/Pages/AroundTheWorld.dart';
+import 'package:dart_assistant/Pages/Dashboard.dart';
+import 'package:dart_assistant/Pages/ThreeHundred.dart';
 import 'package:flutter/material.dart';
+import 'package:dart_assistant/dataModels/NavPage.dart';
+
+import 'Pages/Pig.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -31,22 +38,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'A utility to aid in keeping score for various dart games.'),
+      home: const MyHomePage(
+          title: 'A utility to aid in keeping score for various dart games.'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +54,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Widget _currentPage = const Dashboard();
 
-  void _incrementCounter() {
+  void navToPage(NavPage page) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentPage = switch(page) {
+        NavPage.dashboard => const Dashboard(),
+        NavPage.threeHundred => const ThreeHundred(),
+        NavPage.pig => const Pig(),
+        NavPage.aroundTheWorld => const AroundTheWorld(),
+      };
     });
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Dart Assistant'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        drawer: Drawer(
+            child: ListView(children: [
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+              ),
+              child: (Text(
+                "Navigation",
+                style: TextStyle(
+                    // color: CupertinoColors.systemBackground,
+                    fontSize: 30),
+              ))),
+          ListTile(
+            leading: const Icon(Icons.dashboard_outlined),
+            title: const Text("Dashboard"),
+            // selectedColor: CustomColors.ioRed,
+            onTap: () {
+              navToPage(NavPage.dashboard);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_circle_outline),
+            title: const Text("300"),
+            onTap: () {
+              navToPage(NavPage.threeHundred);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.copy),
+            title: const Text("Pig"),
+            onTap: () {
+              navToPage(NavPage.pig);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.language),
+            title: const Text("Around the world"),
+            onTap: () {
+              navToPage(NavPage.aroundTheWorld);
+            },
+          ),
+        ])),
+        body: _currentPage,
     );
   }
 }
